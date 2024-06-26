f2be0b896e2913439570bfeafd1f45e3b6543268
hieu92264 <104044219+hieu92264@users.noreply.github.com>
2024-06-26 09:16:03 +0700

    done db

--- database/migrations/2024_06_26_013220_create_buyer_sellers_table.php
+++ database/migrations/2024_06_26_013220_create_buyer_sellers_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('buyer_sellers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('address')->nullable();
             $table->string('phone')->nullable();
-            $table->string('password');
+            $table->string('address')->nullable();
+            $table->enum('type', ['buyer', 'seller']);
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('buyer_sellers');
     }
 };
