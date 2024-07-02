4a1e38410f1cc3a2f443e7d0ff9e88e74cdce92a
donghoang1711 <donghoang17112003@gmail.com>
2024-07-02 16:46:11 +0700

    properties_add

--- app/Http/Controllers/properties/PropertiController.php
+++ app/Http/Controllers/properties/PropertiController.php
@@ -3,17 +3,77 @@
 namespace App\Http\Controllers\properties;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CreatePropertiesRequest;
+use App\Models\Location;
+use App\Models\Properties;
+use App\Models\PropertiesDescription;
+use App\Models\PropertyImages;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PropertiController extends Controller
 {
+    public function __construct()
+    {
+
+    }
     public function index()
     {
-        return view('properties.index');
+        $properties = Properties::with(['hasImages', 'hasLocation'])
+            ->latest()
+            ->paginate(6);
+        return view('properties.index', compact('properties'));
     }
 
-    public function create(){
+    public function create()
+    {
         return view('properties.create');
     }
     //
+    public function store(CreatePropertiesRequest $request)
+    {
+        $validatedData = $request->validated();
+
+        $property = Properties::create([
+            'type' => $validatedData['type'],
+            'status' => $validatedData['status'],
+            'created_by_id' => Auth::id(),
+        ]);
+
+        $property->hasDescription()->create([
+            'owner' => $validatedData['owner'],
+            'phone_number' => $validatedData['phone_number'],
+            'gmail' => $validatedData['gmail'],
+            'acreage' => $validatedData['acreage'],
+            'price' => $validatedData['price'],
+            'frontage' => $validatedData['frontage'],
+            'house_direction' => $validatedData['house_direction'],
+            'floors' => $validatedData['floors'],
+            'bedrooms' => $validatedData['bedrooms'],
+            'toilets' => $validatedData['toilets'],
+            'legality' => $validatedData['legality'],
+            'furniture' => $validatedData['furniture'],
+            'other_description' => $validatedData['other_description'],
+        ]);
+
+        if ($request->hasFile('images')) {
+            foreach ($request->file('images') as $image) {
+                $imagePath = $image->store('property_images', 'public'); // Lưu vào thư mục public/property_images
+                $propertyImage = new PropertyImages([
+                    'image_url' => 'property_images/' . basename($imagePath), // Lưu đường dẫn tương đối trong cơ sở dữ liệu
+                ]);
+                $property->hasImages()->save($propertyImage);
+            }
+        }
+        $property->hasLocation()->create([
+            'city' => $validatedData['city'],
+            'district' => $validatedData['district'],
+            'ward' => $validatedData['ward'],
+            'street' => $validatedData['street'],
+            'full_address' => $validatedData['full_address'],
+        ]);
+
+        return redirect()->route('bat-dong-san.index');
+    }
+
 }
--- app/Models/PropertiesDescription.php
+++ app/Models/PropertiesDescription.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class PropertiesDescription extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'property_id',
+        'owner',
+        'phone_number',
+        'gmail',
+        'acreage',
+        'price',
+        'frontage',
+        'house_direction',
+        'floors',
+        'bedrooms',
+        'toilets',
+        'legality',
+        'furniture',
+        'other_description'
+    ];
+    public function hasProperty(): BelongsTo
+    {
+        return $this->belongsTo(Properties::class, 'property_id');
+    }
 }
