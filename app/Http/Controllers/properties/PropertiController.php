27b8eeb1971ca1e44d3c10066748f3cd4c160224
hieu92264 <104044219+hieu92264@users.noreply.github.com>
2024-07-03 15:05:21 +0700

    fix bug search properties

--- app/Http/Controllers/properties/PropertiController.php
+++ app/Http/Controllers/properties/PropertiController.php
@@ -40,7 +40,7 @@ class PropertiController extends Controller
                     ->orWhere("locations.district", "LIKE", "%$search%")
                     ->orWhere("locations.ward", "LIKE", "%$search%")
                     ->orWhere("locations.street", "LIKE", "%$search%")
-                    ->orWhere("Properties.id", "LIKE", "%$search%");
+                    ->orWhere("properties.id", "LIKE", "%$search%");
             });
         }
 
@@ -52,54 +52,54 @@ class PropertiController extends Controller
         }
         switch ($price) {
             case "2":
-                $query->where('properties.price', '<', '500000000');
+                $query->where('properties_descriptions.price', '<', '500000000');
                 break;
             case "3":
-                $query->where('properties.price', '>', '500000000')->where('properties.price', '<', '800000000');
+                $query->where('properties_descriptions.price', '>', '500000000')->where('properties_descriptions.price', '<', '800000000');
                 break;
             case "4":
-                $query->where('properties.price', '>', '800000000')->where('properties.price', '<', '1000000000');
+                $query->where('properties_descriptions.price', '>', '800000000')->where('properties_descriptions.price', '<', '1000000000');
                 break;
             case "5":
-                $query->where('properties.price', '>', '1000000000')->where('properties.price', '<', '2000000000');
+                $query->where('properties_descriptions.price', '>', '1000000000')->where('properties_descriptions.price', '<', '2000000000');
                 break;
             case "6":
-                $query->where('properties.price', '>', '2000000000')->where('properties.price', '<', '3000000000');
+                $query->where('properties_descriptions.price', '>', '2000000000')->where('properties_descriptions.price', '<', '3000000000');
                 break;
             case "7":
-                $query->where('properties.price', '>', '3000000000')->where('properties.price', '<', '5000000000');
+                $query->where('properties_descriptions.price', '>', '3000000000')->where('properties_descriptions.price', '<', '5000000000');
                 break;
             case "8":
-                $query->where('properties.price', '>', '5000000000')->where('properties.price', '<', '7000000000');
+                $query->where('properties_descriptions.price', '>', '5000000000')->where('properties_descriptions.price', '<', '7000000000');
                 break;
             case "9":
-                $query->where('properties.price', '>', '7000000000');
+                $query->where('properties_descriptions.price', '>', '7000000000');
                 break;
         }
         switch ($area) {
             case "2":
-                $query->where('properties.area', '<', '30');
+                $query->where('properties_descriptions.acreage', '<', '30');
                 break;
             case "3":
-                $query->where('properties.area', '>', '30')->where('properties.area', '<', '50');
+                $query->where('properties_descriptions.acreage', '>', '30')->where('properties_descriptions.acreage', '<', '50');
                 break;
             case "4":
-                $query->where('properties.area', '>', '50')->where('properties.area', '<', '80');
+                $query->where('properties_descriptions.acreage', '>', '50')->where('properties_descriptions.acreage', '<', '80');
                 break;
             case "5":
-                $query->where('properties.area', '>', '80')->where('properties.area', '<', '100');
+                $query->where('properties_descriptions.acreage', '>', '80')->where('properties_descriptions.acreage', '<', '100');
                 break;
             case "6":
-                $query->where('properties.area', '>', '100')->where('properties.area', '<', '150');
+                $query->where('properties_descriptions.acreage', '>', '100')->where('properties_descriptions.acreage', '<', '150');
                 break;
             case "7":
-                $query->where('properties.area', '>', '150')->where('properties.area', '<', '200');
+                $query->where('properties_descriptions.acreage', '>', '150')->where('properties_descriptions.acreage', '<', '200');
                 break;
             case "8":
-                $query->where('properties.area', '>', '200')->where('properties.area', '<', '250');
+                $query->where('properties_descriptions.acreage', '>', '200')->where('properties_descriptions.acreage', '<', '250');
                 break;
             case "9":
-                $query->where('properties.area', '>', '250');
+                $query->where('properties_descriptions.acreage', '>', '250');
                 break;
         }
         if ($status !== '1') {
