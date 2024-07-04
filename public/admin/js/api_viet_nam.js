733cd41e1f7c6a775e1d42c10b7d701710ffdd5a
donghoang1711 <donghoang17112003@gmail.com>
2024-07-04 14:20:56 +0700

    api_dia_chi

--- public/admin/js/api_viet_nam.js
+++ public/admin/js/api_viet_nam.js
@@ -1,37 +1,81 @@
 $(document).ready(function () {
-    //Lấy tỉnh thành
+    // Load dữ liệu tỉnh/thành
     $.getJSON('https://esgoo.net/api-tinhthanh/1/0.htm', function (data_tinh) {
         if (data_tinh.error == 0) {
             $.each(data_tinh.data, function (key_tinh, val_tinh) {
-                $("#tinh").append('<option value="' + val_tinh.id + '">' + val_tinh.full_name + '</option>');
+                $("#city").append('<option value="' + val_tinh.id + '" data-name="' +
+                    val_tinh.full_name + '">' + val_tinh.full_name + '</option>');
             });
-            $("#tinh").change(function (e) {
+
+            // Sự kiện chọn tỉnh/thành
+            $("#city").change(function (e) {
+                var cityName = $("#city option:selected").data("name");
+                $("#city_name").val(cityName);
+
                 var idtinh = $(this).val();
-                //Lấy quận huyện
-                $.getJSON('https://esgoo.net/api-tinhthanh/2/' + idtinh + '.htm', function (data_quan) {
+                // Reset dropdown quận/huyện và phường/xã
+                $("#district").html('<option value="0">Quận/Huyện</option>');
+                $("#ward").html('<option value="0">Phường/Xã</option>');
+
+                // Load dữ liệu quận/huyện
+                $.getJSON('https://esgoo.net/api-tinhthanh/2/' + idtinh + '.htm', function (
+                    data_quan) {
                     if (data_quan.error == 0) {
-                        $("#quan").html('<option value="0">Quận Huyện</option>');
-                        $("#phuong").html('<option value="0">Phường Xã</option>');
                         $.each(data_quan.data, function (key_quan, val_quan) {
-                            $("#quan").append('<option value="' + val_quan.id + '">' + val_quan.full_name + '</option>');
-                        });
-                        //Lấy phường xã  
-                        $("#quan").change(function (e) {
-                            var idquan = $(this).val();
-                            $.getJSON('https://esgoo.net/api-tinhthanh/3/' + idquan + '.htm', function (data_phuong) {
-                                if (data_phuong.error == 0) {
-                                    $("#phuong").html('<option value="0">Phường Xã</option>');
-                                    $.each(data_phuong.data, function (key_phuong, val_phuong) {
-                                        $("#phuong").append('<option value="' + val_phuong.id + '">' + val_phuong.full_name + '</option>');
-                                    });
-                                }
-                            });
+                            $("#district").append('<option value="' +
+                                val_quan.id + '" data-name="' + val_quan
+                                    .full_name + '">' + val_quan.full_name +
+                                '</option>');
                         });
 
+                        // Sự kiện chọn quận/huyện
+                        $("#district").change(function (e) {
+                            var districtName = $(
+                                "#district option:selected").data(
+                                    "name");
+                            $("#district_name").val(districtName);
+
+                            var idquan = $(this).val();
+                            // Reset dropdown phường/xã
+                            $("#ward").html(
+                                '<option value="0">Phường/Xã</option>');
+
+                            // Load dữ liệu phường/xã
+                            $.getJSON('https://esgoo.net/api-tinhthanh/3/' +
+                                idquan + '.htm',
+                                function (data_phuong) {
+                                    if (data_phuong.error == 0) {
+                                        $.each(data_phuong.data,
+                                            function (key_phuong,
+                                                val_phuong) {
+                                                $("#ward").append(
+                                                    '<option value="' +
+                                                    val_phuong
+                                                        .id +
+                                                    '" data-name="' +
+                                                    val_phuong
+                                                        .full_name +
+                                                    '">' +
+                                                    val_phuong
+                                                        .full_name +
+                                                    '</option>');
+                                            });
+
+                                        // Sự kiện chọn phường/xã
+                                        $("#ward").change(function (e) {
+                                            var wardName = $(
+                                                "#ward option:selected"
+                                            ).data(
+                                                "name");
+                                            $("#ward_name").val(
+                                                wardName);
+                                        });
+                                    }
+                                });
+                        });
                     }
                 });
             });
-
         }
     });
-});	   
+});
--- resources/views/properties/create.blade.php
+++ resources/views/properties/create.blade.php
@@ -96,26 +96,26 @@
 
                     <div class="form-group">
                         <label for="city">Thành Phố</label>
-                        <select class="form-control" id="city" name="city" title="Chọn Tỉnh Thành">
+                        <select class="form-control" id="city" name="city_id" title="Chọn Tỉnh Thành">
                             <option value="0">Tỉnh Thành</option>
                         </select>
-                        <input type="hidden" name="city_name" id="city_name">
+                        <input type="hidden" name="city" id="city_name">
                     </div>
 
                     <div class="form-group">
                         <label for="district">Quận/Huyện</label>
-                        <select class="form-control" id="district" name="district" title="Chọn Quận Huyện">
+                        <select class="form-control" id="district" name="district_id" title="Chọn Quận Huyện">
                             <option value="0">Quận Huyện</option>
                         </select>
-                        <input type="hidden" name="district_name" id="district_name">
+                        <input type="hidden" name="district" id="district_name">
                     </div>
 
                     <div class="form-group">
                         <label for="ward">Phường/Xã</label>
-                        <select class="form-control" id="ward" name="ward" title="Chọn Phường Xã">
+                        <select class="form-control" id="ward" name="ward_id" title="Chọn Phường Xã">
                             <option value="0">Phường Xã</option>
                         </select>
-                        <input type="hidden" name="ward_name" id="ward_name">
+                        <input type="hidden" name="ward" id="ward_name">
                     </div>
 
                     <div class="form-group">
@@ -140,89 +140,5 @@
             </div>
         </div>
     </div>
-
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            // Load tỉnh/thành data
-            $.getJSON('https://esgoo.net/api-tinhthanh/1/0.htm', function(data_tinh) {
-                if (data_tinh.error == 0) {
-                    $.each(data_tinh.data, function(key_tinh, val_tinh) {
-                        $("#city").append('<option value="' + val_tinh.id + '" data-name="' +
-                            val_tinh.full_name + '">' + val_tinh.full_name + '</option>');
-                    });
-
-                    // Sự kiện chọn tỉnh/thành
-                    $("#city").change(function(e) {
-                        var cityName = $("#city option:selected").data("name");
-                        $("#city_name").val(cityName);
-
-                        var idtinh = $(this).val();
-                        // Reset dropdown quận/huyện và phường/xã
-                        $("#district").html('<option value="0">Quận/Huyện</option>');
-                        $("#ward").html('<option value="0">Phường/Xã</option>');
-
-                        // Load dữ liệu quận/huyện
-                        $.getJSON('https://esgoo.net/api-tinhthanh/2/' + idtinh + '.htm', function(
-                            data_quan) {
-                            if (data_quan.error == 0) {
-                                $.each(data_quan.data, function(key_quan, val_quan) {
-                                    $("#district").append('<option value="' +
-                                        val_quan.id + '" data-name="' + val_quan
-                                        .full_name + '">' + val_quan.full_name +
-                                        '</option>');
-                                });
-
-                                // Sự kiện chọn quận/huyện
-                                $("#district").change(function(e) {
-                                    var districtName = $(
-                                        "#district option:selected").data(
-                                        "name");
-                                    $("#district_name").val(districtName);
-
-                                    var idquan = $(this).val();
-                                    // Reset dropdown phường/xã
-                                    $("#ward").html(
-                                        '<option value="0">Phường/Xã</option>');
-
-                                    // Load dữ liệu phường/xã
-                                    $.getJSON('https://esgoo.net/api-tinhthanh/3/' +
-                                        idquan + '.htm',
-                                        function(data_phuong) {
-                                            if (data_phuong.error == 0) {
-                                                $.each(data_phuong.data,
-                                                    function(key_phuong,
-                                                        val_phuong) {
-                                                        $("#ward").append(
-                                                            '<option value="' +
-                                                            val_phuong
-                                                            .id +
-                                                            '" data-name="' +
-                                                            val_phuong
-                                                            .full_name +
-                                                            '">' +
-                                                            val_phuong
-                                                            .full_name +
-                                                            '</option>');
-                                                    });
-
-                                                // Sự kiện chọn phường/xã
-                                                $("#ward").change(function(e) {
-                                                    var wardName = $(
-                                                        "#ward option:selected"
-                                                    ).data(
-                                                        "name");
-                                                    $("#ward_name").val(
-                                                        wardName);
-                                                });
-                                            }
-                                        });
-                                });
-                            }
-                        });
-                    });
-                }
-            });
-        });
-    </script>
+    <script src="{{ asset('admin/js/api_viet_nam.js') }}"></script>
 @endsection
